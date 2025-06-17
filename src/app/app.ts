5563a9ca977c2a79777db5732cd03312c764dd87
jmlatov <jmlatov@gmail.com>
2025-06-17 22:40:45 +0200

    Añado las páginas base y rutas adicionales: acerca y contacto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header';
-import { Body } from './components/body/body';
+// import { Body } from './components/body/body';
 import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, Body, Footer, RouterModule],
+  imports: [HeaderComponent, Footer, RouterModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css' ]
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import 'zone.js';
 import { AppComponent } from './app/app';
+import { Body } from './app/components/body/body';
+import { Acerca } from './app/pages/acerca/acerca';
+import { Contacto } from './app/pages/contacto/contacto';
+import { provideRouter, Routes } from '@angular/router';
 
-bootstrapApplication(AppComponent)
-  .catch(err => console.error(err));
+const routes: Routes = [
+  { path: '', component: Body },
+  { path: 'acerca', component: Acerca },
+  { path: 'contacto', component: Contacto },
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+}).catch(err => console.error(err));
